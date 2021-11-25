9d925a769acbada16b99c885f8a26ad60642e4f0
Satoshi Warita <stm@stmtok.com>
2021-11-25 11:18:34 +0900

    add generate headers task

--- src/stmtok/cpp/hello.cpp
+++ src/stmtok/cpp/hello.cpp
@@ -1,9 +1,10 @@
 #include <jni.h>
 #include <stdio.h>
+#include "com_stmtok_HelloJNI.h"
 
 JNIEXPORT void JNICALL
 Java_com_stmtok_HelloJNI_print(JNIEnv *env, jobject obj)
 {
-printf("Hello World!\n");
-return;
+    printf("Hello World!\n");
+    return;
 }
